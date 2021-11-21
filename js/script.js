672adcd5b0df41b4044f423425610d49cc18b820
Lum0o <ProGpipart@gmail.com>
2021-11-21 21:47:03 +0100

    easter egg

--- js/script.js
+++ js/script.js
@@ -7,4 +7,17 @@ $(document).ready(function(){
     fullWidth: true,
     indicators: true
   });
+  $("#secret").click(function() {
+    var elem1 = $("#alderiate");
+    var audio = new Audio('audio/alderiate-uwu.m4a');
+
+    elem1.show();
+
+    elem1.animate({top: '-=200px'}, 4000);
+    elem1.animate({top: '-=290px'}, 300);
+    elem1.animate({left: '+=1500px'}, 1500);
+    audio.play();
+    elem1.animate({top: '+=490px'}, 0);
+    elem1.animate({left: '-=1500px'}, 0);
+  });
 });
